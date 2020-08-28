ec378669ad77640982d45ae2c463ff6f91bee41a
Zviad Mgaloblishvili <hausiusi@gmail.com>
2020-08-28 23:56:51 +0200

    Count lines of code from files
    Implemented a basic code line counter. Can distinguish several language
    files and successfully count empty, code and comment lines.
    
    Closes #4
    
    Can ignore some file extensions and directories.
    File extensions and directories can be reset or/and appended using
    optional arguments, hence, can be ignored some directories. e.g. when
    they are third party and user wants to count only own code lines.
    
    Example of usage: python3 run.py -p pat_to_project_root_dir --igndir
    dir1_to_ignore dir2_to_ignore dirn_to_ignore --ignext ext1_to_ignore
    ext2_to_ignore extn_to_ignore
    
    Closes #9

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from models import Commit
 from git import Cmd
 from git import CmdRunner
 from git import Parse
+import loc
 import datetime
 import argparse
 import os
@@ -20,7 +21,7 @@ Gitribution is a tool intended for statistical analyzes of git projects.
 
 def get_commits_by_authors():
     result = CmdRunner.run(Cmd.SHORTLOG)
-    return Parse.commits_by_author(result)
+    return Parse.commits_by_authors(result)
 
 
 def get_stats():
@@ -33,12 +34,23 @@ if __name__ == "__main__":
         epilog=EPILOG, formatter_class=argparse.RawDescriptionHelpFormatter)
     # yapf:disable - disabling formatting while apprending parser arguments to improve readability
     parser.add_argument("-p", "--projdir", metavar="", help="Path to project root directory", default="./")
+    parser.add_argument("-d", "--igndir", metavar="", help="Appends directories that will be ignored", default=[], nargs="+")
+    parser.add_argument("-e", "--ignext", metavar="", help="Appends extensions that will be ignored", default=[], nargs="+")
+    parser.add_argument("--igndir_clear", metavar="", help="Resets default directories that will be ignored", default=False)
+    parser.add_argument("--ignext_clear", metavar="", help="Resets default extensions that will be ignored", default=False)
 
     # yapf:enable
 
     args = parser.parse_args()
     saved_working_dir = os.getcwd()
     os.chdir(args.projdir)
+    if args.igndir_clear:
+        loc.ignored_directories_clear()
+    if args.ignext_clear:
+        loc.ignored_extensions_clear
+
+    loc.ignored_directories_extend(args.igndir)
+    loc.ignored_extensions_extend(args.ignext)
 
     stats = get_stats()
     for aut in stats.authors:
@@ -47,5 +59,13 @@ if __name__ == "__main__":
                   f" {stats.authors[aut].lines_added} lines added" + \
                       f" {stats.authors[aut].lines_deleted} lines deleted")
 
+    file_paths = loc.get_file_names(args.projdir)
+    for f in file_paths:
+        result = loc.CodeFileAnalyzer(f)
+        print(
+            f"File: {f} code: {result.code_lines}, comments: {result.comment_lines}, empty: {result.empty_lines}"
+        )
+
+    print(loc.CodeFileAnalyzer.result)
     # change working directory back to saved one
     os.chdir(saved_working_dir)
