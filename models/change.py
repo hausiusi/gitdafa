e5957174a5350443f9c4a83639dbda8c8adbe364
SaturnR <iraklisivsivadze@gmail.com>
2020-10-31 17:24:08 +0400

    Add docstring
    Add a basic docstring to all modules
    
    Closes #38

--- models/change.py
+++ models/change.py
@@ -1,8 +1,25 @@
 class Change:
+    """
+    Model, to reflect file change properties.
+    
+    Parameters
+    ----------
+    file_name : str, optional
+    added : str, optional
+    deleted : str, optional    
+    
+    Attributes
+    ----------
+    self.file : str
+        File name
+    self.added : int
+    self.deleted : int
+
+    """
     def __init__(self,
                  file_name: str = None,
-                 added: str = None,
-                 deleted: str = None):
+                 added: int = 0,
+                 deleted: int = 0):
         self.file = file_name
         self.added = added
         self.deleted = deleted
