f8dc6e6b5c5884f95804643c6bea8aa66fd32d4f
SaturnR <iraklisivsivadze@gmail.com>
2020-12-01 00:05:09 +0400

    Refactor and modify parser
    Optimize the parser.py using regex methods.
    
    Closes #43

--- git/parser.py
+++ git/parser.py
@@ -3,6 +3,7 @@ from models import Commit
 from models import Author
 from models import Change
 import datetime
+import re
 """
 Docstring for parser module
 
@@ -35,79 +36,89 @@ class Parse:
     Class has private helper method __get_file_change which counts file changes.
     """
     @classmethod
-    def author(cls, lines):
+    def commit_id(cls, text: str):
+        """
+        Get commit ID "40 character hexadecimal number".
+        
+        Parameters
+        ----------
+        test : str
+            Represents commit message string
+        
+        Returns
+        -------
+        str or None
+            Commit ID
+          """
+        result = re.findall("[0-9a-f]{40}", text)
+        if result == []:
+            return None
+        return result[0]
+
+    @classmethod
+    def author(cls, text):
         """
         Get author of the commit.
         
         Parameters
         ----------
-        lines : list of str
-            Represents all data lines in a one commit message.
+        text : str
+            Represents all data in a one commit message.
         
         Returns
         -------
-        Author 
+        Author or None
             Commit author object
         """
         name = None
         email = None
-        for line in lines:
-            if line[:8] == "Author: ":
-                data = line[8:].split(" <")
-                name = data[0]
-                email = data[1][:-1]
-                break
+        #print('-------')
+
+        result = re.findall(r"Author:.*", text)
+        if len(result) == 0:
+            return None
+        name = re.sub("(Author:.)|(.<.*>)", "", result[0])
+        email = re.sub("(Author:.*<)|(>)", "", result[0])
         return Author(name=name, email=email)
 
     @classmethod
-    def date(cls, lines: list):
+    def date(cls, text: str):
         """
         Get data of the commits.
 
         Parameters
         ----------
-        lines : list of str
-            Represents all data lines in a one commit message.
+        text : list of str
+            Represents all data in a one commit message.
         
         Returns
         -------
-        datetime.datetime
+        datetime.datetime or None
             Commit date
         """
-        for line in lines:
-            if line[:6] == "Date: ":
-                date = datetime.datetime.strptime(line[6:].strip(),
-                                                  '%a %b %d %H:%M:%S %Y %z')
-                return date
+        line = re.findall("Date:.*", text)
+        if len(line) == 0:
+            return None
+        date = datetime.datetime.strptime(line[0][6:].strip(),
+                                          '%a %b %d %H:%M:%S %Y %z')
+        return date
 
     @classmethod
-    def message(cls, lines: list):
+    def message(cls, text: str):
         """
         Get commit message 
         
         Parameters
         ----------
-        lines : list of str
-            Represents all data lines in a one commit message.
+        text : str
+            Represents all data in a one commit message.
         
         Returns
         -------
         str
             Message string
         """
-        after_date = False
-        empty_count = 0
-        message = ""
-        for line in lines:
-            if after_date:
-                message += line + '\n'
-                if len(line) == 0:
-                    empty_count += 1
-                if empty_count == 2:
-                    return message
-            if line[:6] == "Date: ":
-                after_date = True
-                continue
+        return "".join(re.findall(r"\n*\s{4}.*", text))
 
     @classmethod
     def __get_file_change(cls, file_text):
@@ -135,35 +146,23 @@ class Parse:
         return Change(file_name=file_name, added=added, deleted=deleted)
 
     @classmethod
-    def changes(cls, lines: list):
+    def changes(cls, text: str):
         """
         Get count of the line change in committed files
         
         Parameters
         ----------
-        lines : list of str
-            Represents all data lines in a one commit message.
+        lines : str
+            Represents all data in a one commit message.
         
         Returns
         -------
         list of Change
             Changes in commit
         """
-        after_date = False
-        after_message = False
-        empty_count = 0
         changes = []
-        for line in lines[:-1]:
-            if after_message:
-                changes.append(cls.__get_file_change(line))
-            if after_date:
-                if len(line) == 0:
-                    empty_count += 1
-                if empty_count == 2:
-                    after_message = True
-            if line[:6] == "Date: ":
-                after_date = True
-                continue
+        for line in re.finditer("\d*\t\d*\t.*", text):
+            changes.append(cls.__get_file_change(line.group(0)))
         return changes
 
     @classmethod
@@ -205,17 +204,14 @@ class Parse:
         """
         stats = Statistics()
         commit_texts = cmd_output.stdout.split("\ncommit ")
-
         for commit_text in commit_texts:
-            commit_lines = commit_text.split('\n')
-            commit_id = commit_lines[0].strip()
+            commit_id = cls.commit_id(commit_text)
             commit = Commit(commit_id)
-            date = cls.date(commit_lines)
+            date = cls.date(commit_text)
             commit.date = str(date)
-            commit.message = cls.message(commit_lines)
-
-            commit.changes = cls.changes(commit_lines)
-            author = cls.author(commit_lines)
+            commit.message = cls.message(commit_text)
+            commit.changes = cls.changes(commit_text)
+            author = cls.author(commit_text)
 
             if author.email not in stats.authors:
                 stats.authors[author.email] = author
