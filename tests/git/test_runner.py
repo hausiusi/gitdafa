b6fa4fa8e851ed11f4b2c583bac4cf908fa2e0ed
Zviad Mgaloblishvili <hausiusi@gmail.com>
2021-02-12 19:07:24 +0100

    Make CmdRunner test Windows compatible
    Closes #53

--- tests/git/test_runner.py
+++ tests/git/test_runner.py
@@ -5,12 +5,16 @@ sys.path.append('/')
 
 from git import CmdRunner
 
+
 def test_run():
     ''' Test on stdout without error'''
-    cmd_out = CmdRunner.run('echo hello|sha256sum', exit_on_fail=False)
+    if sys.platform == "win32":
+        cmd = 'echo 5891b5b522d5df086d0ff0b110fbd9d21bb4fc7163af34d08286a2e846f6be03 > 5891b5b522_45.txt & cat 5891b5b522_45.txt'
+    else:
+        cmd = 'echo hello|sha256sum'
+    cmd_out = CmdRunner.run(cmd, exit_on_fail=False)
     assert cmd_out.success
-    assert '5891b5b522d5df086d0ff0b110fbd9d21bb4fc7163af34d08286a2e846f6be03' in cmd_out.stdout  
+    assert '5891b5b522d5df086d0ff0b110fbd9d21bb4fc7163af34d08286a2e846f6be03' in cmd_out.stdout
     ''' Test on stderror'''
-    cmd_out = CmdRunner.run('rm 5891b5b522_45.txt', exit_on_fail=False)
+    cmd_out = CmdRunner.run('rm 5891b5b522_45.txt 2e846f6be03_67.txt', exit_on_fail=False)
     assert not cmd_out.success
-
